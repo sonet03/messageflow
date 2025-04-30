e1480a4fa232159357e3d6f0ccf44171db92ebb5
sonet <osafroniuk@gmail.com>
2025-04-30 19:58:39 +0300

    Add batch block processing strategy

--- MessageFlow.Kafka/Strategies/BatchProcessingStrategy.cs
+++ MessageFlow.Kafka/Strategies/BatchProcessingStrategy.cs
@@ -9,9 +9,30 @@ namespace MessageFlow.Kafka.Strategies
 {
     public class BatchProcessingStrategy<TMessage> : IProcessingStrategy<TMessage>
     {
+        private readonly BatchBlock<MessageEnvelope<TMessage>> _batchBlock;
+        private readonly ActionBlock<IEnumerable<MessageEnvelope<TMessage>>> _runBlock;
+
+        public BatchProcessingStrategy(Func<IEnumerable<MessageEnvelope<TMessage>>, Task> handle, int batchSize = 100, int batchTimeoutMs = 1000)
+        {
+            _batchBlock = new BatchBlock<MessageEnvelope<TMessage>>(batchSize,
+                new GroupingDataflowBlockOptions
+                {
+                    BoundedCapacity = 100,
+                    EnsureOrdered = true
+                });
+            _runBlock = new ActionBlock<IEnumerable<MessageEnvelope<TMessage>>>(handle,
+                new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = 1,
+                    BoundedCapacity = 100
+                });
+
+            _batchBlock.LinkTo(_runBlock);
+        }
+
         public Task DispatchAsync(MessageEnvelope<TMessage> message)
         {
-            return Task.CompletedTask;
+            return _batchBlock.SendAsync(message);
         }
     }
 } 
