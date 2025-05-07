454666fff7b043f837e947111996bd7990e7887f
sonet <osafroniuk@gmail.com>
2025-05-07 18:57:43 +0300

    Add KafkaMessagePublisher.cs

--- MessageFlow.Kafka/KafkaMessagePublisher.cs
+++ MessageFlow.Kafka/KafkaMessagePublisher.cs
@@ -1,11 +1,43 @@
 using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
 
 namespace MessageFlow.Kafka
 {
     public class KafkaMessagePublisher<TMessage> : IMessagePublisher<TMessage>, IDisposable
     {
+        private readonly string _topic;
+        private readonly Func<TMessage, byte[]> _serializer;
+        private readonly ILogger<KafkaMessagePublisher<TMessage>> _logger;
+        private readonly IProducer<string, byte[]> _producer;
+        
+        public KafkaMessagePublisher(ProducerConfig producerConfig, string topic, Func<TMessage, byte[]> serializer, ILogger<KafkaMessagePublisher<TMessage>> logger)
+        {
+            _producer = new ProducerBuilder<string, byte[]>(producerConfig).Build();
+            _topic = topic;
+            _serializer = serializer;
+            _logger = logger;
+        }
+        
+        public async Task PublishAsync(string key, TMessage message)
+        {
+            try
+            {
+                var result =
+                    await _producer.ProduceAsync(_topic,
+                        new Message<string, byte[]> { Key = key, Value = _serializer(message) });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to publish message for message with {Key}", key);
+            }
+        }
+        
         public void Dispose()
         {
+            _producer.Flush(TimeSpan.FromSeconds(5));
+            _producer.Dispose();
         }
     }
 }
--- MessageFlow/IMessagePublisher.cs
+++ MessageFlow/IMessagePublisher.cs
@@ -1,7 +1,9 @@
+using System.Threading.Tasks;
+
 namespace MessageFlow
 {
     public interface IMessagePublisher<TMessage>
     {
-        
+        public Task PublishAsync(string key, TMessage message);
     }
 }
